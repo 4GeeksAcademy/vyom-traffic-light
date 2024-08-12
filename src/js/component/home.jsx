f468964d848a059364c6019c0bf06b670d115646
Amat98 <milton.yv98@gmail.com>
2024-08-12 16:06:31 -0500

    practicando react con el useState

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [activeColor, setActiveColor] = useState(null);
+  const [newColor, setNewColor] = useState(false);
+
+  const handleColorClick = (color) => {
+    setActiveColor(activeColor === color ? null : color);
+    console.log(`hola ${color}`);
+  };
+
+  const handleNewColor = () => {
+    setNewColor(!newColor);
+  };
+
+  return (
+    <div
+      className="bg-secondary text-white d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <h1 className="fw-bold">Semaforo</h1>
+      <div className="bg-black" style={{ width: "10px", height: "70px" }}></div>
+      <div className="bg-black rounded p-2" style={{ width: "120px" }}>
+        <div
+          className="bg-danger mx-auto"
+          style={{
+            width: "100px",
+            height: "100px",
+            borderRadius: "100%",
+            boxShadow:
+              activeColor === "red" ? "red 0px 10px 90px 40px" : "none",
+          }}
+          onClick={() => handleColorClick("red")}
+        ></div>
+        <div
+          className="bg-warning mx-auto"
+          style={{
+            width: "100px",
+            height: "100px",
+            borderRadius: "100%",
+            boxShadow:
+              activeColor === "yellow" ? "yellow 0px 10px 90px 40px" : "none",
+          }}
+          onClick={() => handleColorClick("yellow")}
+        ></div>
+        <div
+          className="bg-success mx-auto"
+          style={{
+            width: "100px",
+            height: "100px",
+            borderRadius: "100%",
+            boxShadow:
+              activeColor === "green" ? "green 0px 10px 90px 40px" : "none",
+          }}
+          onClick={() => handleColorClick("green")}
+        ></div>
+        <div
+          className=" mx-auto"
+          style={{
+            background: "violet",
+            width: "100px",
+            height: "100px",
+            borderRadius: "100%",
+            boxShadow:
+              activeColor === "purpure" ? "violet 0px 10px 90px 40px" : "none",
+            display: newColor ? "block" : "none",
+          }}
+          onClick={() => handleColorClick("purpure")}
+        ></div>
+      </div>
+      <button className="btn bg-primary mt-5" onClick={handleNewColor}>
+        {newColor ? "Quitar Purpura" : "Agregar purpura"}
+      </button>
+    </div>
+  );
 };
 
 export default Home;
